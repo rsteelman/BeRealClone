2dab98532b3ceaeeb6fc7ec94f15e68984430208
Robert Steelman <rmichael.steelman@gmail.com>
2025-09-21 13:18:37 -0400

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by Michael Steelman on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeRealClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeRealClone
+//
+//  Created by Michael Steelman on 9/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
